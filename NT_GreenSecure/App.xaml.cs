8c30e6b863fbf99e7f43d8c05cf5d12e1309747a
Fillraen <nt.aergie@gmail.com>
2023-09-12 23:51:48 +0200

    Ajoutez des fichiers projet.

---
+++ NT_GreenSecure/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using NT_GreenSecure.Services;
+using NT_GreenSecure.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NT_GreenSecure
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ NT_GreenSecure/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using NT_GreenSecure.ViewModels;
+using NT_GreenSecure.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace NT_GreenSecure
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
