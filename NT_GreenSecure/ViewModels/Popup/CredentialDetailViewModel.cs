ad59a5a830058c6c1cd686d4dbfa169bd0818318
Nicolas <NT.Aergie@gmail.com>
2023-09-25 07:29:02 +0200

    a

---
+++ NT_GreenSecure/ViewModels/Popup/CredentialDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NT_GreenSecure.ViewModels.Popup
+{
+    public class CredentialDetailViewModel : BaseViewModel
+    {
+        public CredentialDetailViewModel()
+        {
+            Title = "Credential Detail";
+        }
+    }
+}
